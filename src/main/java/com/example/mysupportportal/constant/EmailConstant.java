66b2c7e132780f2722781bfd88e5a42280d2321d
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-03-07 23:43:36 +0100

    added gmail credencial as env vars

--- src/main/java/com/example/mysupportportal/constant/EmailConstant.java
+++ src/main/java/com/example/mysupportportal/constant/EmailConstant.java
@@ -2,8 +2,6 @@ package com.example.mysupportportal.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "zorand666@gmail.com";
-    public static final String PASSWORD = "majmunce1";
     public static final String FROM_EMAIL = "support@getarrays.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
--- src/main/java/com/example/mysupportportal/service/EmailService.java
+++ src/main/java/com/example/mysupportportal/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.example.mysupportportal.service;
 
 import com.sun.mail.smtp.SMTPTransport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
@@ -17,16 +18,19 @@ import static com.example.mysupportportal.constant.EmailConstant.*;
 @Service
 public class EmailService {
 
+    @Value("${email.username}")
+    private String USERNAME;
+
+    @Value("${email.password}")
+    private String PASSWORD;
+
     public void sendNewPasswordEmail(String firstName, String password, String email) throws MessagingException {
         Message message=createEmail(firstName,password,email);
         SMTPTransport smtpTransport= (SMTPTransport) getEmailSession().getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
-        smtpTransport.connect(GMAIL_SMTP_SERVER,USERNAME,PASSWORD);
+        smtpTransport.connect(GMAIL_SMTP_SERVER,USERNAME, PASSWORD);
         smtpTransport.sendMessage(message,message.getAllRecipients());
         smtpTransport.close();
     }
-
-
-
 
     private Message createEmail(String firstName, String password, String email) throws MessagingException {
         Message message=new MimeMessage(getEmailSession());
