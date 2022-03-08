24e31c1cc5f78e4ecc8f6846d3bb7b8a3d48b53b
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-03-08 20:38:51 +0100

    added /,/login, /register to open

--- src/main/java/com/example/mysupportportal/constant/SecurityConstant.java
+++ src/main/java/com/example/mysupportportal/constant/SecurityConstant.java
@@ -11,7 +11,7 @@ public class SecurityConstant {
     public static final String FORBIDDEN_MESSAGE="Ypu need to login to access this page or page does not exist";
     public static final String ACCESS_DENIED_MESSAGE="You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD="OPTIONS";
-    public static final String[] PUBLIC_URLS={"/user/login", "/user/register", "/user/image/**"};
+    public static final String[] PUBLIC_URLS={"/","/login","/register","/user/login", "/user/register", "/user/image/**"};
     //public static final String[] PUBLIC_URLS={"**"};
 
 }
